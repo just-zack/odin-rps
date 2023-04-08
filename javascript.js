2d028c89708e351796e544445d3bedca426657bd
Zach_Huang <128434708+zach-huang@users.noreply.github.com>
2023-04-08 12:15:00 -1000

    Create game (); Fix errors with global variables

--- javascript.js
+++ javascript.js
@@ -1,11 +1,7 @@
-
-let playerSelection = getPlayerSelection ()
-let computerSelection = getComputerChoice ();
-
 let playerScore = 0;
 let computerScore = 0;
-let resultString = "";
-let numberOfGames = 2;
+let playersChoice = ""
+let computersChoice = ""
 
 function getComputerChoice () {
     let gameChoices = ["Rock", "Paper", "Scissors"];
@@ -13,74 +9,70 @@ function getComputerChoice () {
     return randChoice;
 }
 
-function getPlayerSelection () {
+function getPlayerChoice () {
     let userChoice = prompt ("Please enter Rock, Paper, or Scissors");
-    userChoice = userChoice.toLowerCase();
-    return userChoice
+    if (userChoice === null) {
+        return userChoice;
+    } else {
+        userChoice = userChoice.toLowerCase();
+        return userChoice;
 }
-
+}
 function playRound (playerSelection, computerSelection) {
     if (playerSelection === "rock" && computerSelection === "Paper") {
         computerScore++;
-        resultString = "You Lose! Paper beats Rock";
-        return resultString;
+        playersChoice = playerSelection.toUpperCase();
+        computersChoice = computerSelection.toUpperCase();
+        return "You Lose! Paper beats Rock";
     } else if (playerSelection === "rock" && computerSelection === "Rock") {
-        resultString = "You Tie! Rock ties Rock";
-        return resultString;
+        playersChoice = playerSelection.toUpperCase();
+        computersChoice = computerSelection.toUpperCase();
+        return "You Tie! Rock ties Rock";
     } else if (playerSelection === "rock" && computerSelection === "Scissors") {
         playerScore++;
-        resultString = "You Win! Rock beats Scissors";
-        return resultString;
+        playersChoice = playerSelection.toUpperCase();
+        computersChoice = computerSelection.toUpperCase();
+        return "You Win! Rock beats Scissors";
     } else if (playerSelection === "scissors" && computerSelection === "Paper") {
         playerScore++;
-        resultString = "You Win! Scissors beats Paper"
-        return resultString;
+        playersChoice = playerSelection.toUpperCase();
+        computersChoice = computerSelection.toUpperCase();
+        return "You Win! Scissors beats Paper";
     } else if (playerSelection === "scissors" && computerSelection === "Rock") {
         computerScore++;
-        resultString = "You Lose! Rock beats Scissors";
-        return resultString
+        playersChoice = playerSelection.toUpperCase();
+        computersChoice = computerSelection.toUpperCase();
+        return "You Lose! Rock beats Scissors";
     } else if (playerSelection === "scissors" && computerSelection === "Scissors") {
-        resultString = "You Tie! Scissors tie Scissors";
-        return resultString;
+        playersChoice = playerSelection.toUpperCase();
+        computersChoice = computerSelection.toUpperCase();
+        return "You Tie! Scissors tie Scissors";
     } else if (playerSelection === "paper" && computerSelection === "Paper") {
-        resultString = "You Tie! Paper ties Paper";
-        return resultString;
+        playersChoice = playerSelection.toUpperCase();
+        computersChoice = computerSelection.toUpperCase();
+        return "You Tie! Paper ties Paper"
     } else if (playerSelection === "paper" && computerSelection === "Rock") {
         playerScore++;
-        resultString = "You Win! Paper beats Rock";
-        return resultString;
+        playersChoice = playerSelection.toUpperCase();
+        computersChoice = computerSelection.toUpperCase();
+        return "You Win! Paper beats Rock"
     } else if (playerSelection === "paper" && computerSelection === "Scissors") {
         computerScore++;
-        resultString = "You Lose! Scissors beats Paper";
-        return resultString;
+        playersChoice = playerSelection.toUpperCase();
+        computersChoice = computerSelection.toUpperCase();
+        return "You Lose! Scissors beats Paper"
+    } else if (playerSelection === null) {
+        return "Please choose Rock, Paper, or Scissors"
     } else return "Please choose Rock, Paper, or Scissors"
 }
 
-function game (playerSelection, computerSelection) {
-    playRound (playerSelection, computerSelection);
-    playRound (playerSelection, computerSelection);
-    playRound (playerSelection, computerSelection);
-    playRound (playerSelection, computerSelection);
-
+function game () {
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = getPlayerChoice ();
+        let computerSelection = getComputerChoice ();
+        console.log(playRound (playerSelection, computerSelection));
+        console.log(playersChoice, computersChoice);
+        console.log(playerScore, computerScore);
+    }
 }
-
-console.log (game (playerSelection, computerSelection));
-console.log (playerScore);
-console.log (computerScore);
-
-
- 
-/*
- function gameTally () {
-    let computerWinCount = 0;
-    let userWinCount = 0;
-}
-
-function game() {
-    for (let i=1; i <= 5; i++) {
-    playRound(playerSelection, computerSelection); }
-
-}
-*/
-
-
+game()
